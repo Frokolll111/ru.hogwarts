a5a142c33ccf8bcb6da0cc3470a838909f59f69d
Frokolll111 <buther1433@gmail.com>
2023-08-01 16:39:41 +0700

    Commit

---
+++ src/main/java/ru/hogwarts/schooll/SchoollApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.schooll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/hogwarts/schooll/SchoollApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.schooll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
